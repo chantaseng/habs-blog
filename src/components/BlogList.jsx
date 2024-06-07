caf89c22e8f81c9498fb1c1d6ce41160cc0b3d93
William Chan <chan.william.ts@gmail.com>
2024-06-07 17:30:50 -0600

    adding useFetch as custom hook

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,6 +1,6 @@
 import { Button } from '@chakra-ui/react';
 
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
   return (
     <div>
       <h2 className="text-4xl font-bold p-4">{title}</h2>
@@ -8,9 +8,9 @@ const BlogList = ({ blogs, title, handleDelete }) => {
         <div key={blog.id} className="p-4">
           <h2 className="text-2xl font-bold text-rose-400">{blog.title}</h2>
           <span className="mr-3">Written by {blog.author}</span>
-          <Button onClick={() => handleDelete(blog.id)} bg="#fb7185">
+          {/* <Button onClick={() => handleDelete(blog.id)} bg="#fb7185">
             Delete
-          </Button>
+          </Button> */}
         </div>
       ))}
     </div>
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,36 +1,18 @@
-import { useState } from 'react';
 import BlogList from './BlogList';
+import useFetch from '../useFetch';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    {
-      title: 'Slavkovsky is set to breakout next season',
-      body: 'lorem ipsum...',
-      author: 'William C.',
-      id: 1,
-    },
-    {
-      title: 'Caufield expected to reach 50 goals next year',
-      body: 'lorem ipsum...',
-      author: 'Khaliun B.',
-      id: 2,
-    },
-    {
-      title: 'Suzuki 80 points next year?',
-      body: 'lorem ipsum...',
-      author: 'William C.',
-      id: 3,
-    },
-  ]);
-
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
+  const {
+    data: blogs,
+    isLoading,
+    error,
+  } = useFetch('http://localhost:8000/blogs');
 
   return (
     <div className="max-w-7xl mx-auto py-6 px-8">
-      <BlogList blogs={blogs} title="All Blogs" handleDelete={handleDelete} />
+      {error && <div>{error}</div>}
+      {isLoading && <div>...Loading</div>}
+      {blogs && <BlogList blogs={blogs} title="All Blogs" />}
     </div>
   );
 };
