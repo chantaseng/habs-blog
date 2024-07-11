eefb52fe331430e38f8b4b8d004536d363a7321c
William Chan <chan.william.ts@gmail.com>
2024-07-11 18:19:28 -0400

    install mongoose and connect app to mongodb database

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const mongoose = require('mongoose');
 const blogRoutes = require('./routes/blogRoute');
 
 // express app
@@ -12,7 +13,17 @@ app.use(express.json());
 // route
 app.use('/api/blogs', blogRoutes);
 
-// listen for request
-app.listen(process.env.PORT, () => {
-  console.log(`listening on port ${process.env.PORT}!!`);
-});
+// connect to db
+mongoose
+  .connect(process.env.MONGO_DB_URI)
+  .then(() => {
+    // listen for request
+    app.listen(process.env.PORT, () => {
+      console.log(
+        `connected to db and listening on port ${process.env.PORT}!!`
+      );
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
