2bf94201ebde112435158a36b123408db52ac81d
William Chan <chan.william.ts@gmail.com>
2024-08-26 12:52:38 -0400

    creating models and schema

--- backend/routes/blogRoute.js
+++ backend/routes/blogRoute.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const Blog = require('../models/blog');
 
 const router = express.Router();
 
@@ -9,12 +10,19 @@ router.get('/', (req, res) => {
 
 // get a single blog
 router.get('/:id', (req, res) => {
-  res.json({ message: 'GET a single blod' });
+  res.json({ message: 'GET a single blog' });
 });
 
 // create a blog
-router.post('/', (req, res) => {
-  res.json({ message: 'CREATE a new blog' });
+router.post('/', async (req, res) => {
+  const { title, body, author } = req.body;
+
+  try {
+    const blog = await Blog.create({ title, body, author });
+    res.status(200).json(blog);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
 });
 
 // delete a blog
